1cb887ab3f97828080bc2f1b6cdd23c3a81c5bf5
Rasmus Veldemann <rass@SCENER-123456.>
2025-02-09 11:29:24 +0200

    ecosystem change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  apps : [{
-    name: "main-frontend",
-    script: 'npm run dev',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-  }],
+    apps : [{
+        name: "main-frontend",
+        script: 'npm run dev',
+        instances: 1,
+        autorestart: true,
+        watch: false,
+        max_memory_restart: '1G',
+    }],
 
-  deploy : {
-    production : {
-      user : 'deployer',
-      host : '68.183.219.8',
-      ref  : 'origin/master',
-      repo : 'git@207.154.253.206:/var/git/main-frontend.git',
-      path : '/var/www/main-frontend/',
-      'pre-deploy-local': '',
-      'post-deploy': 'docker compose -p main-frontend build && docker compose -p main-frontend stop && docker compose -p main-frontend up -d',
-      'pre-setup': ''
+    deploy : {
+        production : {
+            user : 'deployer',
+            host : '159.89.108.231',
+            ref  : 'origin/master',
+            repo : 'git@github.com:Rassuke1232/main-frontend.git',
+            path : '/var/www/main-frontend/',
+            'pre-deploy-local': '',
+            'post-deploy': 'docker compose -p main-frontend build && docker compose -p main-frontend stop && docker compose -p main-frontend up -d',
+            'pre-setup': ''
+        }
     }
-  }
 };
